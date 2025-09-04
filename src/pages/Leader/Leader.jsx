fcb4fc4b752c905ea752934540d79bcb9153d2da
YOU <amwayzack@gmail.com>
2025-09-04 12:41:32 +0800

    data skelton

--- src/pages/Leader/Leader.jsx
+++ src/pages/Leader/Leader.jsx
@@ -3,6 +3,7 @@ import style from './Leader.module.css';
 function Leader(){
     return (
         <div className={style.placeholder}>Leader Placeholder</div>
+        // TODO: 讀取 introduction.markdown
     )
 }
 
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,6 +3,7 @@ import style from "./Projects.module.css"
 function Projects(){
     return (
         <div className={style.placeholder}>Projects Placeholder</div>
+        // TODO: 讀取 XXXX.markdown
     )
 }
 
