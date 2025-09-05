35755ae3583db06b421a5c5b3a34d70cddfc938b
YOU <amwayzack@gmail.com>
2025-09-06 00:14:40 +0800

    Completing Header (Except for color scheme)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,39 @@
 import { Link } from "react-router-dom";
 import style from "./Header.module.css";
+import short_logo from "../../assets/short_logo.png";
+import Contact from "../gadgets/Contact";
 
 function Header() {
-  return (
-    <>
-      <Link to="/">
-        <div>Homepage</div>
+  return (<>
+  <div className={style.header} >
+
+    {/* Left side */}
+    <div className={style.nav_bar}>
+      <Link to="/" >
+        <img src={short_logo} alt= "short_logo"/>
       </Link>
-      <Link to="/Leader">
-        <div>Leader</div>
+      <Link to="/Projects" >
+        <div className="inter-bold">Projects</div>
       </Link>
-      <Link to="/Members">
-        <div>Members</div>
+      <Link to="/Members" >
+        <div className="inter-bold">Members</div>
+      </Link>      
+      <Link to="/Leader" >
+        <div className="inter-bold">Leader</div>
       </Link>
-      <Link to="/News">
-        <div>News</div>
+      <Link to="/News" >
+        <div className="inter-bold">News</div>
       </Link>
-      <Link to="/Projects">
-        <div>Projects</div>
-      </Link>
-    </>
-  );
+
+    </div>
+
+    {/* Right side */}
+    <div>
+      <Contact/>
+    </div>
+
+  </div>
+  </>);
 }
 
 export default Header;
