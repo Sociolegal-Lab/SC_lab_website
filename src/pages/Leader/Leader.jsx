533f31f824ef0e290107e122908cb5178ab1489f
thomaschem9329 <147964694+thomaschem9329@users.noreply.github.com>
2025-11-13 05:25:03 +0800

    leader collapse bug fixed

--- src/pages/Leader/Leader.jsx
+++ src/pages/Leader/Leader.jsx
@@ -4,7 +4,7 @@ import FellowshipsAndAwardsCollapse from  "./FellowshipsAndAwardsCollapse"
 import PublicationCollapse from "./PublicationCollapse"
 import InternationalConferenceCollapse from "./InternationalConferenceCollapse"
 import PastSpeakingEngagementsCollapse from "./PastSpeakingEngagementsCollapse.jsx"
-import UpcomingSpeakingEngagementsCollapse from "./OngoingProjectsCollapse.jsx"
+import UpcomingSpeakingEngagementsCollapse from "./UpcomingSpeakingEngagementsCollapse.jsx"
 import OngoingProjectsCollapse from "./OngoingProjectsCollapse.jsx"
 
 
