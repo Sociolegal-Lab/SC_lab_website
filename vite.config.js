6f64a4788ba274b13e59c47a2f9dbcddf53be93b
Smile <chenforwork1368@gmail.com>
2025-11-14 16:19:50 +0800

    Adjust base url

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig(({ command }) => {
-    return {
-        plugins: [react()],
-        base: command = '/SC_lab_website/',
-    };
-});
+export default defineConfig({
+    plugins: [react()],
+    base: '/', // ✅ 以根路徑部署
+})
