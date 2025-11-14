6f751f9bf846640f410aa374775eaea1953a735e
thomaschem9329 <147964694+thomaschem9329@users.noreply.github.com>
2025-11-14 10:25:30 +0800

    About Us icon added

--- src/pages/Homepage/AboutUs.jsx
+++ src/pages/Homepage/AboutUs.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import styles from "./Homepage.module.css";
 import "../../styles/font.css";
+import dataImg from "../../data/homepage/DATA.png";
+import lawImg from "../../data/homepage/LAW.png";
+import societyImg from "../../data/homepage/SOCIETY.png";
 
 export default function AboutUs() {
   return (
     <section className={styles["mr-about"]}>
       <div className={styles["mr-about__inner"]}>
-        {/* 標題原本在 CSS 設 700，可加 inter-bold 交由 font.css 控制（可選） */}
         <h2 className={`inter-bold ${styles["mr-about__title"]}`}>About Us</h2>
 
         <div className={styles["mr-about__grid"]}>
-          {/* DATA */}
           <div className={styles["mr-about__item"]}>
-            <img src="/images/circle.png" alt="circle" className={styles["mr-shape-img"]} />
+            <img src={dataImg} alt="DATA" className={styles["mr-shape-img"]} />
             <div className={`inter-extrabold ${styles["mr-about__label"]}`}>DATA</div>
             <div className={`inter-bold ${styles["mr-about__lines"]}`}>
               <div>50465406845</div>
@@ -22,9 +23,8 @@ export default function AboutUs() {
             </div>
           </div>
 
-          {/* LAW */}
           <div className={styles["mr-about__item"]}>
-            <img src="/images/triangle.png" alt="triangle" className={styles["mr-shape-img"]} />
+            <img src={lawImg} alt="LAW" className={styles["mr-shape-img"]} />
             <div className={`inter-extrabold ${styles["mr-about__label"]}`}>LAW</div>
             <div className={`inter-bold ${styles["mr-about__lines"]}`}>
               <div>50465406845</div>
@@ -33,10 +33,9 @@ export default function AboutUs() {
               <div>000</div>
             </div>
           </div>
-
-          {/* SOCIETY */}
+          
           <div className={styles["mr-about__item"]}>
-            <img src="/images/star.png" alt="star" className={styles["mr-shape-img"]} />
+            <img src={societyImg} alt="SOCIETY" className={styles["mr-shape-img"]} />
             <div className={`inter-extrabold ${styles["mr-about__label"]}`}>SOCIETY</div>
             <div className={`inter-bold ${styles["mr-about__lines"]}`}>
               <div>50465406845</div>
--- src/pages/Members/Thelabinaction.jsx
+++ src/pages/Members/Thelabinaction.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import styles from "./Members.module.css"; // ← 正確載入 CSS Modules
+import styles from "./Members.module.css"; 
+import thelabinactionImg from "../../data/carousel/2.jpg";
 
 export default function Thelabinaction() {
   return (
     <div className={`rufina-bold ${styles["members-section"]}`}>
       <h2>The Lab In Action</h2>
       <br />
-      <div className={styles["members-picture"]}>
-        <img
-          src="https://pbs.twimg.com/media/GzSnuLdb0AASEjf?format=jpg&name=4096x4096"
-          alt="The Lab in Action"
+      <img
+        src={thelabinactionImg}
+        alt="The Lab in Action"
+        className={styles["members-picture"]}
         />
-      </div>
     </div>
   );
 }
